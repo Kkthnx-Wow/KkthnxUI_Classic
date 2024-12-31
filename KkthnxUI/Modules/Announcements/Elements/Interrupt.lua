d0c719cad6b02aefec62a8a9325035f9fefdfc58
Kkthnx <40672673+Kkthnx@users.noreply.github.com>
2024-12-31 01:54:31 -0500

    Interrupt and ItemAlert
    😫

--- KkthnxUI/Modules/Announcements/Elements/Interrupt.lua
+++ KkthnxUI/Modules/Announcements/Elements/Interrupt.lua
@@ -2,48 +2,38 @@ local K, C, L = KkthnxUI[1], KkthnxUI[2], KkthnxUI[3]
 local Module = K:GetModule("Announcements")
 
 -- Localize API functions
-local string_format, GetInstanceInfo, C_Spell_GetSpellLink, IsActiveBattlefieldArena, IsArenaSkirmish, IsInGroup, IsInRaid, IsPartyLFG, UnitInParty, UnitInRaid = string.format, GetInstanceInfo, C_Spell.GetSpellLink, IsActiveBattlefieldArena, IsArenaSkirmish, IsInGroup, IsInRaid, IsPartyLFG, UnitInParty, UnitInRaid
+local string_format, GetInstanceInfo, IsActiveBattlefieldArena, IsArenaSkirmish, IsInGroup, IsInRaid, IsPartyLFG, UnitInParty, UnitInRaid = string.format, GetInstanceInfo, IsActiveBattlefieldArena, IsArenaSkirmish, IsInGroup, IsInRaid, IsPartyLFG, UnitInParty, UnitInRaid
 
 local AURA_TYPE_BUFF = AURA_TYPE_BUFF
 local infoType = {}
 
-local brokenBlackList = {
-	[102359] = true,
-	[105421] = true,
-	[115191] = true,
-	[122] = true,
-	[157997] = true,
-	[1776] = true,
-	[1784] = true,
-	[197214] = true,
-	[198121] = true,
-	[207167] = true,
-	[207685] = true,
-	[226943] = true,
-	[228600] = true,
-	[285515] = true,
-	[31661] = true,
-	[331866] = true,
-	[33395] = true,
-	[354051] = true,
-	[355689] = true,
-	[378760] = true,
-	[386770] = true,
-	[5246] = true,
-	[64695] = true,
-	[8122] = true,
-	[82691] = true,
-	[91807] = true,
-	[99] = true,
+local blackList = {
+	[(GetSpellInfo(99))] = true, -- 夺魂咆哮
+	[(GetSpellInfo(122))] = true, -- 冰霜新星
+	[(GetSpellInfo(1776))] = true, -- 凿击
+	[(GetSpellInfo(1784))] = true, -- 潜行
+	[(GetSpellInfo(5246))] = true, -- 破胆怒吼
+	[(GetSpellInfo(8122))] = true, -- 心灵尖啸
 }
 
-local interruptBlackList = {
-	[31935] = true,
+local LOCspells = {
+	[(GetSpellInfo(853))] = true, -- 制裁之锤
+	[(GetSpellInfo(1776))] = true, -- 凿击
+	[(GetSpellInfo(2070))] = true, -- 闷棍
+	[(GetSpellInfo(2094))] = true, -- 致盲
+	[(GetSpellInfo(5246))] = true, -- 破胆怒吼
+	[(GetSpellInfo(5782))] = true, -- 恐惧
+	[(GetSpellInfo(8122))] = true, -- 心灵尖啸
+	[(GetSpellInfo(14308))] = true, -- 冰冻陷阱
+	[(GetSpellInfo(15487))] = true, -- 沉默
+	[(GetSpellInfo(19386))] = true, -- 翼龙钉刺
+	[(GetSpellInfo(19503))] = true, -- 驱散射击
+	[(GetSpellInfo(20066))] = true, -- 忏悔
 }
 
 local function getAlertChannel()
 	local _, instanceType = GetInstanceInfo()
-	local inPartyLFG = IsPartyLFG() or C_PartyInfo.IsPartyWalkIn()
+	local inPartyLFG = IsPartyLFG()
 	local inRaid = IsInRaid()
 
 	if instanceType == "arena" then
@@ -86,47 +76,53 @@ function Module:InterruptAlert_IsEnabled()
 end
 
 function Module:IsAllyPet(sourceFlags)
-	return K.IsMyPet(sourceFlags) or sourceFlags == K.PartyPetFlags or sourceFlags == K.RaidPetFlags
+	if K.IsMyPet(sourceFlags) or sourceFlags == K.PartyPetFlags or sourceFlags == K.RaidPetFlags then
+		return true
+	end
 end
 
 function Module:InterruptAlert_Update(...)
-	local _, eventType, _, sourceGUID, sourceName, sourceFlags, _, _, destName, _, _, spellID, _, _, extraskillID, _, _, auraType = ...
+	local _, eventType, _, sourceGUID, sourceName, sourceFlags, _, destGUID, destName, _, _, _, spellName, _, _, extraskillName, _, auraType = ...
 	if not sourceGUID or sourceName == destName then
 		return
 	end
 
-	local isPlayerOrAllyPet = sourceName == K.Name or Module:IsAllyPet(sourceFlags)
-
-	if (UnitInRaid(sourceName) or UnitInParty(sourceName) or isPlayerOrAllyPet) and infoType[eventType] then
-		local infoText = infoType[eventType]
-		local sourceSpellID, destSpellID
-
-		if infoText == L["Broken Spell"] then
-			if auraType == AURA_TYPE_BUFF or brokenBlackList[spellID] then
-				return
-			end
-			sourceSpellID, destSpellID = extraskillID, spellID
-		elseif infoText == L["Interrupt"] then
-			if (C["Announcements"].OwnInterrupt and not isPlayerOrAllyPet) or interruptBlackList[spellID] then
-				return
-			end
-			sourceSpellID, destSpellID = spellID, extraskillID
-		else
-			if C["Announcements"].OwnDispell and not isPlayerOrAllyPet then
-				return
-			end
-			sourceSpellID, destSpellID = spellID, extraskillID
+	local LoCAlert = true
+	if LoCAlert and eventType == "SPELL_AURA_APPLIED" and LOCspells[spellName] and destGUID == K.GUID then
+		local duration = select(5, AuraUtil_FindAuraByName(spellName, "player", "HARMFUL"))
+		if duration > 1.5 then
+			SendChatMessage(string_format("LoC - %s > %s (%ss %s)", sourceName .. "[" .. spellName .. "]", destName, duration, GetMinimapZoneText()), getAlertChannel())
 		end
+	elseif UnitInRaid(sourceName) or UnitInParty(sourceName) or Module:IsAllyPet(sourceFlags) then
+		local infoText = infoType[eventType]
+		if infoText then
+			local sourceSpellID, destSpellID
+			if infoText == L["Broken Spell"] then
+				if auraType and auraType == AURA_TYPE_BUFF or blackList[spellName] then
+					return
+				end
+				sourceSpellID, destSpellID = extraskillName, spellName
+			elseif infoText == L["Interrupt"] then
+				if C["Announcements"].OwnInterrupt and sourceName ~= K.Name and not K.IsMyPet(sourceFlags) then
+					return
+				end
+				sourceSpellID, destSpellID = spellName, extraskillName
+			else
+				if C["Announcements"].OwnDispell and sourceName ~= K.Name and not K.IsMyPet(sourceFlags) then
+					return
+				end
+				sourceSpellID, destSpellID = spellName, extraskillName
+			end
 
-		if sourceSpellID and destSpellID then
-			local message = infoText == L["Broken Spell"] and string_format(infoText, sourceName, C_Spell_GetSpellLink(destSpellID)) or string_format(infoText, C_Spell_GetSpellLink(destSpellID))
-			SendChatMessage(message, getAlertChannel())
+			if sourceSpellID and destSpellID then
+				SendChatMessage(string_format(infoText, sourceName .. "[" .. sourceSpellID .. "]", destName .. "[" .. destSpellID .. "]"), getAlertChannel())
+			end
 		end
 	end
 end
 
 function Module:InterruptAlert_CheckGroup()
-	if IsInGroup() and (not C["Announcements"].InstAlertOnly or (IsInInstance() and not IsPartyLFG() or C_PartyInfo.IsPartyWalkIn())) then
+	if IsInGroup() and (not C["Announcements"].InstAlertOnly or IsInInstance()) then
 		K:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED", Module.InterruptAlert_Update)
 	else
 		K:UnregisterEvent("COMBAT_LOG_EVENT_UNFILTERED", Module.InterruptAlert_Update)
